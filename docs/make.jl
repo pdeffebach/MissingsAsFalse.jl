ed2fe7606363fe77f83c63d8a19e213ab00e2775
Peter Deffebach <p.deffebach@gmail.com>
2021-10-16 07:52:05 -0400

    better make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -2,4 +2,17 @@ push!(LOAD_PATH,"../src/")
 
 using Documenter, MissingsAsFalse
 
-makedocs(sitename="MissingsAsFalse.jl Documentation")
+makedocs(
+    sitename = "MissingsAsFalse.jl",
+    pages = Any[
+        "Introduction" => "index.md",
+        "API" => "api/api.md"],
+    format = Documenter.HTML(
+        canonical = "https://pdeffebach.github.io/MissingsAsFalse.jl/stable/"
+    ))
+
+deploydocs(
+    repo = "github.com/pdeffebach/MissingsAsFalse.jl.git",
+    target = "build",
+    deps = nothing,
+    make = nothing)
